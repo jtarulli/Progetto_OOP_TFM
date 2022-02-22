eb92100d39e49f7f9748333f3233461d577a93d6
Jacopo Tarulli <s1099259@studenti.univpm.it>
2022-02-22 16:36:25 +0100

    Interfaccia del service, commenti

--- src/main/java/it/univpm/OOPProject_TMF/service/Service.java
+++ src/main/java/it/univpm/OOPProject_TMF/service/Service.java
@@ -1,8 +1,28 @@
 package it.univpm.OOPProject_TMF.service;
 
 
+import it.univpm.OOPProject_TMF.exception.CustomedException;
+import it.univpm.OOPProject_TMF.model.Location;
+import it.univpm.OOPProject_TMF.model.LocationWithCoordinates;
+import it.univpm.OOPProject_TMF.model.Metadata;
+import it.univpm.OOPProject_TMF.model.Stats;
+
+import java.util.List;
+
+/**
+ * Interfaccia per visualizzare i metodi del Service
+ * @author jacopotarulli
+ *
+ */
 
 @org.springframework.stereotype.Service
 public interface Service {
+	
+	public abstract List<Metadata> MetadataList();
+	public abstract List<Location> DataList();
+	public abstract List<LocationWithCoordinates> DataList(String placeName, double distance) throws CustomedException;
+	public abstract List<Location> DataListCC(String countryCode) throws CustomedException;
+	public abstract List<Location> TrendsAvailable();
+	public abstract List<Stats> Stats();
 
 }
