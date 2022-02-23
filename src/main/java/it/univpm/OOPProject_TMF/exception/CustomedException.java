59e1d0247b3a352da12df5d6846fd6497fee92e3
Federico Mennecozzi <s1100693@studenti.univpm.it>
2022-02-23 23:35:55 +0100

    Implementazione della classe CustomedException, commenti

---
+++ src/main/java/it/univpm/OOPProject_TMF/exception/CustomedException.java
@@ -0,0 +1,17 @@
+package it.univpm.OOPProject_TMF.exception;
+
+/**
+ * La classe CustomedException gestisce le eccezioni per il file location.txt o per input errato
+ * 
+ * @author Federico Mennecozzi
+ */
+public class CustomedException extends  Exception {
+	
+	private static final long serialVersionUID = 1L;
+
+	public CustomedException(String message) {
+		super(message);
+		System.out.print("Bad Data Input");
+	}
+
+}
